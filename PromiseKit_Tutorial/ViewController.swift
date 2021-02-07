0818fcb7bbcdc1f5372c660e5f08243e5aaed0e0
Jeff, Jeong  정 의정 <35421421+TuenTuenna@users.noreply.github.com>
2021-02-08 00:54:05 +0900

    프로미스킷 적용

--- PromiseKit_Tutorial/ViewController.swift
+++ PromiseKit_Tutorial/ViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import PromiseKit
 
 enum MyError : Error {
     case iHateLaundary, iHateCleanMyRoom
@@ -99,16 +99,85 @@ class ViewController: UIViewController {
         })
     }
     
+    fileprivate func doHeavyWorkWithReturn(_ jobTitle: String, duration: Float) -> Promise<String>{
+        print("jobTitle: \(jobTitle) 시작")
+        return Promise { seal in
+            Timer.scheduledTimer(withTimeInterval: TimeInterval(duration), repeats: false, block: { _ in
+                print("\(jobTitle) 완료")
+                switch jobTitle {
+                case "빨래하기":
+                    seal.reject(MyError.iHateLaundary)
+                case "내방청소":
+                    seal.reject(MyError.iHateCleanMyRoom)
+                default:
+                    seal.fulfill("\(jobTitle) 완료")
+                }
+            })
+        }
+    }
+    
+    
     fileprivate func doLaundary(){
         print("doLaundary() called")
+        firstly{
+            doHeavyWorkWithReturn("손빨래하기", duration: 1.0)
+        }.then{ result in
+            self.doHeavyWorkWithReturn("내방청소", duration: 1.0)
+        }.then{ result in
+            self.doHeavyWorkWithReturn("\(result) 하고 설거지", duration: 1.0)
+        }.done{ result in
+            self.showAlert(vc: self, title: result)
+        }.catch{ error in
+            print("에러가 발생하였다! / error: \(error)")
+        }
     }
     
     fileprivate func goShopping(){
         print("goShopping() called")
+        
+        firstly{
+            when(fulfilled: doHeavyWorkWithReturn("청소하기", duration: 1.0),
+                doHeavyWorkWithReturn("손빨래하기", duration: 1.0),
+                doHeavyWorkWithReturn("빨래건조하기", duration: 1.0))
+        }.done{ first, second, third in
+            self.showAlert(vc: self, title: "\(first), \(second), \(third) 모두 완료!")
+        }.catch{ error in
+            print("에러가 발생하였다! / error: \(error)")
+        }
     }
     
     fileprivate func goPicnic(){
         print("goPicnic() called")
+        firstly{
+            self.doHeavyWorkWithReturn("내방청소", duration: 1.0)
+        }
+        .then { result in
+            when(fulfilled: self.doHeavyWorkWithReturn("창고청소하기", duration: 1.0),
+                 self.doHeavyWorkWithReturn("손빨래하기", duration: 1.0),
+                 self.doHeavyWorkWithReturn("빨래건조하기", duration: 1.0))
+        }
+        .map{ first, second, third in
+            return "완료한 것들: \(first), \(second), \(third)"
+        }
+        .recover{ error -> Promise<String> in
+            guard error as! MyError == MyError.iHateCleanMyRoom else {
+                throw error
+            }
+            return after(seconds: 2.0).then{
+                self.doHeavyWorkWithReturn("내방청소하기 싫어도 해", duration: 1.0)
+            }
+        }
+        .done{ result in
+            self.showAlert(vc: self, title: "\(result) 모두 완료!")
+        }
+        .ensure {
+            print("이것은 무조건 실행된다")
+        }
+        .catch{ error in
+            print("에러가 발생하였다! / error: \(error)")
+        }
+        
+        
     }
     
     
